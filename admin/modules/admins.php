93ccd188ee91c2e6e9a615d9567d76efaac30c03
Eduard Laas <info@slaed.net>
2025-12-04 17:21:13 +0100

    Refactor: admins.php - All inconsistencies resolved Änderungen: 1. Funktion navi() → adminsNavi() (consistent with other modules) 2. Navigation: name=admins&op=show → name=admins (use default case) 3. Formular: Redundant Hidden-Field "name" removed (in Action-URL) 4. Funktion getAdminInfo() → adm_info() (konsistent) 5. Redirects: Explicit &op=show added for clarity All function calls updated: - admins() Zeile 18 - add() Zeile 54 - info() Zeile 167

--- admin/modules/admins.php
+++ admin/modules/admins.php
@@ -6,8 +6,8 @@
 
 if (!defined('ADMIN_FILE') || !is_admin_god()) die('Illegal file access');
 
-function navi(int $opt = 0, int $tab = 0, int $subtab = 0, int $legacy = 0): string {
-    $ops = ['name=admins&amp;op=show', 'name=admins&amp;op=add', 'name=admins&amp;op=info'];
+function adminsNavi(int $opt = 0, int $tab = 0, int $subtab = 0, int $legacy = 0): string {
+    $ops = ['name=admins', 'name=admins&amp;op=add', 'name=admins&amp;op=info'];
     $lang = [_HOME, _ADD, _INFO];
     return getAdminTabs(_EDITADMINS, 'admins.png', '', $ops, $lang, [], [], $tab, $subtab);
 }
@@ -15,7 +15,7 @@ function navi(int $opt = 0, int $tab = 0, int $subtab = 0, int $legacy = 0): str
 function admins(): void {
     global $prefix, $db, $admin_file, $conf;
     head();
-    $cont = navi(0, 0, 0, 0);
+    $cont = adminsNavi(0, 0, 0, 0);
     if (getVar('get', 'send', 'num')) $cont .= setTemplateWarning('warn', ['time' => '', 'url' => '', 'id' => 'info', 'text' => _MAIL_SEND]);
     $cont .= setTemplateBasic('open');
     $cont .= '<table class="sl_table_list_sort"><thead><tr><th>'._NICKNAME.'</th><th>'._URANK.'</th><th>'._URL.'</th><th>'._EMAIL.'</th><th>'._LANGUAGE.'</th><th>'._IP.'</th><th class="{sorter: false}">'._FUNCTIONS.'</th></tr></thead><tbody>';
@@ -51,12 +51,12 @@ function add(): void {
         $lang = getVar('post', 'lang', '', $conf['language']);
     }
     head();
-    $cont = navi(0, 1, 0, 0);
+    $cont = adminsNavi(0, 1, 0, 0);
     if ($stop) $cont .= setTemplateWarning('warn', ['time' => '', 'url' => '', 'id' => 'warn', 'text' => $stop]);
     $check = (empty($_COOKIE['sl_close_9'])) ? '' : ' checked';
     $cont .= setTemplateBasic('open');
-    $cont .= '<form name="post" action="'.$admin_file.'.php" method="post">'
-    .'<input type="hidden" name="name" value="admins"><input type="hidden" name="op" value="save">'
+    $cont .= '<form name="post" action="'.$admin_file.'.php?name=admins" method="post">'
+    .'<input type="hidden" name="op" value="save">'
     .'<table class="sl_table_form">'
     .'<tr><td>'._NICKNAME.':</td><td>'.get_user_search('adminname', $name, '25', 'sl_form', '1').'</td></tr>'
     .'<tr><td>'._URANK.':</td><td><input type="text" name="title" value="'.$title.'" maxlength="50" class="sl_form" placeholder="'._URANK.'"></td></tr>'
@@ -149,7 +149,7 @@ function save(): void {
             mail_send($email, $conf['adminmail'], $subject, $msg, 0, 3);
             $send = '&send=1';
         }
-        header('Location: '.$admin_file.'.php?name=admins'.$send);
+        header('Location: '.$admin_file.'.php?name=admins&op=show'.$send);
     } else {
         add();
     }
@@ -159,12 +159,12 @@ function del(): void {
     global $prefix, $db, $admin_file;
     $id = getVar('get', 'id', 'num');
     $db->sql_query('DELETE FROM '.$prefix.'_admins WHERE id = :id', ['id' => $id]);
-    header('Location: '.$admin_file.'.php?name=admins');
+    header('Location: '.$admin_file.'.php?name=admins&op=show');
 }
 
 function info(): void {
     head();
-    echo navi(0, 2, 0, 0).'<div id="repadm_info">'.getAdminInfo(1, 0, 'admins').'</div>';
+    echo adminsNavi(0, 2, 0, 0).'<div id="repadm_info">'.adm_info(1, 0, 'admins').'</div>';
     foot();
 }
 
