301f24f0fedb3ba7adb95d4e235cc93cbd0807fd
Eduard Laas <info@slaed.net>
2025-12-10 12:52:25 +0100

    Cleanup and Configuration Update: Remove legacy files, untrack system configurations (.gitignore, .editorconfig), and fix changelog.

--- config/changelog.php
+++ config/changelog.php
@@ -1,19 +1,15 @@
 <?php
 # Author: Eduard Laas
-# Copyright Â© 2005 - 2025 SLAED
+# Copyright Â 2005 - 2025 SLAED
 # License: GNU GPL 3
 # Website: slaed.net
 
 if (!defined('FUNC_FILE')) die('Illegal file access');
 
 $conflog = array (
-  'export_enabled' => '1',
-  'github_owner' => 'SLAED-CMS',
-  'github_repo' => 'SLAED-CMS-6.3-Pro',
-  'github_token' => '', // TODO: Set via environment variable or config
   'group_by_date' => '1',
-  'limit' => '50',
-  'per_page' => '10',
+  'limit' => '500',
+  'per_page' => '50',
   'show_files' => '1',
   'show_stats' => '1',
   'source' => 'github',
