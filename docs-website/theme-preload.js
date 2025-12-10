83dacde7327057441c0863af6dbc0c960a172b86
Eduard Laas <info@slaed.net>
2025-12-10 12:52:25 +0100

    Cleanup and Configuration Update: Remove legacy files, untrack system configurations (.gitignore, .editorconfig), and fix changelog.

--- docs-website/theme-preload.js
+++
@@ -1,7 +0,0 @@
-// Apply theme immediately to prevent flash of white
-(function() {
-    const savedTheme = localStorage.getItem('theme');
-    const osPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const initialTheme = savedTheme === 'dark' || (!savedTheme && osPrefersDark) ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-theme', initialTheme);
-})();
--- setup.php
+++ setup.php
@@ -1,9 +1,10 @@
 <?php
 # Author: Eduard Laas
-# Copyright © 2005 - 2019 SLAED
+# Copyright © 2005 - 2026 SLAED
 # License: GNU GPL 3
 # Website: slaed.net
 
 define('SETUP_FILE', true);
-include('setup/setup.php');
-?>
+$sgtime = microtime(true);
+define('BASE_DIR', str_replace('\\', '/', __DIR__));
+require_once BASE_DIR.'/setup/setup.php';
