02dd8f8077da8657945f01ce9e49b60a0be667f7
Eduard Laas <info@slaed.net>
2025-12-12 11:52:51 +0100

    Refactor: Optimize replace.php with modern patterns
    Updated replace.php with comprehensive modernization:
    - Updated copyright year to 2026
    - Added require_once CONFIG_DIR at top for config loading
    - Enhanced navi() function with legacy and extra parameters
    - Modernized navigation ops with proper name=replace URLs
    - Updated template functions to use array parameters
    - Replaced end_chmod() with checkConfigFile() helper
    - Complete rewrite of save() function using getVar() for input validation
    - Implemented setConfigFile() for config writing (replacing manual file operations)
    - Added global $confre variable handling
    - Simplified and secured all user input handling
    - Compact switch statement formatting

--- admin/modules/replace.php
+++ admin/modules/replace.php
@@ -1,94 +1,89 @@
 <?php
 # Author: Eduard Laas
-# Copyright © 2005 - 2018 SLAED
+# Copyright © 2005 - 2026 SLAED
 # License: GNU GPL 3
 # Website: slaed.net
 
 if (!defined('ADMIN_FILE') || !is_admin_god()) die('Illegal file access');
+require_once CONFIG_DIR.'/replace.php';
 
-function navi(int $opt = 0, int $tab = 0, int $subtab = 0): string {
-	$ops = ($opt == 1) ? ['replace', 'replace', 'replace_info'] : ['', '', 'replace_info'];
-	$lang = [_CONTENT, _NEWS, _INFO];
-	return getAdminTabs(_REPLACE, 'replace.png', '', $ops, $lang, [], [], $tab, (bool)$subtab);
+function navi(int $opt = 0, int $tab = 0, int $subtab = 0, int $legacy = 0, string $extra = ''): string {
+    $ops = ($opt == 1) ? ['name=replace', 'name=replace', 'name=replace&amp;op=info'] : ['', '', 'name=replace&amp;op=info'];
+    $lang = [_CONTENT, _NEWS, _INFO];
+    return getAdminTabs(_REPLACE, 'replace.png', '', $ops, $lang, [], [], $tab, $subtab, $legacy, $extra);
 }
 
 function replace(): void {
-	global $aroute;
-	head();
-	$cont = navi(0, 0, 0, 0);
-	$cont .= setTemplateWarning('warn', _REPLACEINFO, '', '', 'info');
-	include('config/replace.php');
-	$permtest = end_chmod('config/replace.php', 666);
-	if ($permtest) $cont .= setTemplateWarning('warn', $permtest, '', '', 'warn');
-	$mods = ['content', 'news'];
-	$content = '';
-	$k = 0;
-	foreach ($mods as $val) {
-		if ($val != '') {
-			$content .= '<div id="tabc'.$k.'" class="tabcont">';
-			$fieldc = explode('||', $confre[$val]);
-			for ($c = 0; $c < 50; $c++) {
-				preg_match('#(.*)\|(.*)#i', $fieldc[$c], $out);
-				$b = $c + 1;
-				$display = (empty($out[1]) && empty($out[1][$c]) != '0' && $c != '0') ? ' class="sl_none"' : '';
-				$hr = ($c == '0') ? '' : '<hr>';
-				$content .= '<div id="fi'.$k.$c.'"'.$display.'>'.$hr
-				.'<table class="sl_table_conf">'
-				.'<tr><td><a OnClick="HideShow(\'fi'.$k.$b.'\', \'slide\', \'up\', 500);" title="'._ADD.'" class="sl_plus">'._REPLACE_FIELD.': '.$b.'</a></td><td>'
-				.'<table><tr><td>'._WORD.':</td><td><input type="text" name="field1'.$k.'[]" value="'.$out[1].'" class="sl_conf" placeholder="'._WORD.'" required></td></tr>'
-				.'<tr><td>'._CONTENT.':<div class="sl_small">'._REPLACEIN.'</div></td><td><textarea name="field2'.$k.'[]" cols="65" rows="5" class="sl_conf" placeholder="'._CONTENT.'" required>'.$out[2].'</textarea></td></tr></table></td>'
-				.'</tr></table></div>';
-			}
-			$content .= '</div>';
-			$k++;
-		}
-	}
-	$cont .= setTemplateBasic('open');
-	$cont .= '<form action="'.$aroute.'.php" method="post">'.$content.'<table class="sl_table_conf"><tr><td class="sl_center"><input type="hidden" name="op" value="replace_save_conf"><input type="submit" value="'._SAVECHANGES.'" class="sl_but_blue"></td></tr></table></form>'
-	.'<script>
-		var countries=new ddtabcontent("replace")
-		countries.setpersist(true)
-		countries.setselectedClassTarget("link")
-		countries.init()
-	</script>';
-	$cont .= setTemplateBasic('close', '');
-	echo $cont;
-	foot();
+    global $aroute, $confre;
+    head();
+    $cont = navi(0, 0, 0, 0, 'replace');
+    $cont .= setTemplateWarning('warn', ['time' => '', 'url' => '', 'id' => 'info', 'text' => _REPLACEINFO]);
+    $cont .= checkConfigFile('replace.php');
+    $mods = ['content', 'news'];
+    $content = '';
+    $k = 0;
+    foreach ($mods as $val) {
+        if ($val != '') {
+            $content .= '<div id="tabc'.$k.'" class="tabcont">';
+            $fieldc = explode('||', $confre[$val]);
+            for ($c = 0; $c < 50; $c++) {
+                preg_match('#(.*)\|(.*)#i', $fieldc[$c], $out);
+                $b = $c + 1;
+                $display = (empty($out[1]) && empty($out[1][$c]) != '0' && $c != '0') ? ' class="sl_none"' : '';
+                $hr = ($c == '0') ? '' : '<hr>';
+                $content .= '<div id="fi'.$k.$c.'"'.$display.'>'.$hr
+                .'<table class="sl_table_conf">'
+                .'<tr><td><a OnClick="HideShow(\'fi'.$k.$b.'\', \'slide\', \'up\', 500);" title="'._ADD.'" class="sl_plus">'._REPLACE_FIELD.': '.$b.'</a></td><td>'
+                .'<table><tr><td>'._WORD.':</td><td><input type="text" name="field1'.$k.'[]" value="'.$out[1].'" class="sl_conf" placeholder="'._WORD.'" required></td></tr>'
+                .'<tr><td>'._CONTENT.':<div class="sl_small">'._REPLACEIN.'</div></td><td><textarea name="field2'.$k.'[]" cols="65" rows="5" class="sl_conf" placeholder="'._CONTENT.'" required>'.$out[2].'</textarea></td></tr></table></td>'
+                .'</tr></table></div>';
+            }
+            $content .= '</div>';
+            $k++;
+        }
+    }
+    $cont .= setTemplateBasic('open');
+    $cont .= '<form action="'.$aroute.'.php" method="post">'.$content.'<table class="sl_table_conf"><tr><td class="sl_center"><input type="hidden" name="name" value="replace"><input type="hidden" name="op" value="save"><input type="submit" value="'._SAVECHANGES.'" class="sl_but_blue"></td></tr></table></form>'
+    .'<script>
+        var countries=new ddtabcontent("replace")
+        countries.setpersist(true)
+        countries.setselectedClassTarget("link")
+        countries.init()
+    </script>';
+    $cont .= setTemplateBasic('close');
+    echo $cont;
+    foot();
 }
 
-function save_conf_data(): void {
-	global $aroute;
-	include('config/replace.php');
-	$content = "\$confre = array();\n";
-	$mods = ['content', 'news'];
-	$a = 0;
-	foreach ($mods as $val) {
-		if ($val != '') {
-			$fields = '';
-			for ($i = 0; $i < 50; $i++) {
-				$ident = ($i == 0) ? '' : '||';
-				$field1 = ($_POST['field1'.$a][$i] != '') ? $_POST['field1'.$a][$i] : 0;
-				$field2 = ($_POST['field2'.$a][$i] != '') ? $_POST['field2'.$a][$i] : 0;
-				$fields .= $ident.$field1.'|'.$field2;
-			}
-			$a++;
-			$content .= "\$confre['".$val."'] = \"".$fields."\";\n";
-		}
-	}
-	save_conf('config/replace.php', $content);
-	header('Location: '.$aroute.'.php?op=replace');
-	exit;
+function save(): void {
+    global $aroute;
+    $cont = [];
+    $mods = ['content', 'news'];
+    $a = 0;
+    foreach ($mods as $val) {
+        $fields = '';
+        for ($i = 0; $i < 50; $i++) {
+            $ident = ($i == 0) ? '' : '||';
+            $field1 = getVar('post', 'field1'.$a.'['.$i.']', 'word', '0') ?: 0;
+            $field2 = getVar('post', 'field2'.$a.'['.$i.']', '', '0') ?: 0;
+            $fields .= $ident.$field1.'|'.$field2;
+        }
+        $a++;
+        $cont[$val] = $fields;
+    }
+    setConfigFile('replace.php', 'confre', $cont);
+    header('Location: '.$aroute.'.php?name=replace');
+    exit;
 }
 
 function info(): void {
-	head();
-	echo navi(1, 2, 0, 0).'<div id="repadm_info">'.adm_info(1, 0, 'replace').'</div>';
-	foot();
+    head();
+    echo navi(1, 2, 0, 0).'<div id="repadm_info">'.adm_info(1, 0, 'replace').'</div>';
+    foot();
 }
 
 switch ($op) {
-	default: replace(); break;
-	case 'replace_save_conf': save_conf_data(); break;
-	case 'replace_info': info(); break;
+    default: replace(); break;
+    case 'save': save(); break;
+    case 'info': info(); break;
 }
-?>
