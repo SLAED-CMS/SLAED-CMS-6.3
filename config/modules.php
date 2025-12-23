2c57637421ec6992f509e456458bba270c215da1
Eduard Laas <info@slaed.net>
2025-12-23 15:22:17 +0100

    Setup: Add generated module configuration file
    - Create config/modules.php with all module settings
    - Migrate module data from database to configuration
    - Include settings for 26 modules: active, view, inmenu, group, side, top
    - Generated by migrateModulesDbToConfig() function

---
+++ config/modules.php
@@ -0,0 +1,253 @@
+<?php
+# Author: Eduard Laas
+# Copyright © 2005 - 2025 SLAED
+# License: GNU GPL 3
+# Website: slaed.net
+
+if (!defined('FUNC_FILE')) die('Illegal file access');
+
+$confmd = array (
+  'account' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '2',
+    'top' => '0',
+  ),
+  'auto_links' => 
+  array (
+    'active' => '0',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'changelog' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'clients' => 
+  array (
+    'active' => '1',
+    'view' => '1',
+    'inmenu' => '1',
+    'group' => '6',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'contact' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'content' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '0',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'faq' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'files' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'forum' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '2',
+    'top' => '0',
+  ),
+  'help' => 
+  array (
+    'active' => '1',
+    'view' => '1',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'image' => 
+  array (
+    'active' => '0',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'jokes' => 
+  array (
+    'active' => '0',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'links' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'main' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '3',
+    'top' => '3',
+  ),
+  'media' => 
+  array (
+    'active' => '0',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'money' => 
+  array (
+    'active' => '0',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'news' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'order' => 
+  array (
+    'active' => '0',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'pages' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'recommend' => 
+  array (
+    'active' => '1',
+    'view' => '1',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'rss_info' => 
+  array (
+    'active' => '1',
+    'view' => '1',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'search' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'shop' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'sitemap' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '2',
+    'top' => '0',
+  ),
+  'users' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'voting' => 
+  array (
+    'active' => '1',
+    'view' => '0',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+  'whois' => 
+  array (
+    'active' => '1',
+    'view' => '1',
+    'inmenu' => '1',
+    'group' => '0',
+    'side' => '0',
+    'top' => '0',
+  ),
+);
