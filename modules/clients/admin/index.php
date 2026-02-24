998ca3bbb4a8b9bf53c8f195ca0360106c9ed3bd
Eduard Laas <info@slaed.net>
2026-02-24 22:52:02 +0100

    Refactor: Modernize clients admin module
    Rewrites the clients admin module using standard SLAED conventions:
    canonical function names, getVar() input handling, prepared SQL
    statements, and modern template calls. SQL injection in save/delete
    queries eliminated. Missing exit after redirects added.
    
    Core changes:
    
    1. Clients admin (modules/clients/admin/index.php):
    - Replace clients_navi() with navi() using getAdminTabs()
    - Rename functions to short canonical names:
      * clients_add()    -> add()
      * clients_save()   -> save()
      * clients_delete() -> del()
      * clients_active() -> status()
      * clients_info()   -> info()
    - Replace all raw $_POST/$_GET with getVar()
    - Fix SQL injection in save(): prepared statements with named placeholders
    - Replace tpl_eval()/tpl_warn() with setTemplateBasic()/setTemplateWarning()
    - Replace $admin_file with $afile throughout
    - Add exit after header() in del() and status()
    - Rewrite switch to compact form with named cases
    - Add missing newline at end of file
    
    Benefits:
    - SQL injection in save and delete queries eliminated
    - Input sanitized via getVar() throughout
    - Missing exit after redirect now present
    - Consistent naming convention across admin modules
    
    Technical notes:
    - op= route values changed to match new function names
    - Backward compatible with the database schema

--- modules/clients/admin/index.php
+++ modules/clients/admin/index.php
@@ -6,147 +6,141 @@
 
 if (!defined('ADMIN_FILE') || !is_admin_modul('clients')) die('Illegal file access');
 
-function clients_navi() {
-	panel();
-	$narg = func_get_args();
-	$ops = array('clients', 'clients_add', 'clients_info');
-	$lang = array(_HOME, _ADD, _INFO);
-	return navi_gen(_CLIENTSA, 'clients.png', '', $ops, $lang, '', '', $narg[0], $narg[1], $narg[2], $narg[3]);
+function navi(int $opt = 0, int $tab = 0, int $subtab = 0, int $legacy = 0): string {
+    $ops = ['name=clients', 'name=clients&amp;op=add', 'name=clients&amp;op=info'];
+    $lang = [_HOME, _ADD, _INFO];
+    return getAdminTabs(_CLIENTSA, 'clients.png', '', $ops, $lang, [], [], $tab, $subtab);
 }
 
-function clients() {
-	global $db, $admin_file, $stop;
-	head();
-	$cont = clients_navi(0, 0, 0, 0);
-	if ($stop) $cont .= tpl_warn("warn", _CERROR, "", "","warn");
-	$result = $db->sql_query("SELECT id, title, infotext, url, num, hits, prod_id, status FROM ".PREFIX_DB."_clients_down");
-	if ($db->sql_numrows($result) > 0) {
-		$cont .= tpl_eval("open");
-		$cont .= "<table class=\"sl_table_list_sort\"><thead><tr><th>"._ID."</th><th>"._CTITLE."</th><th>"._CVERSION."</th><th>"._CDATE."</th><th>"._ID."</th><th>"._CLOADS."</th><th class=\"{sorter: false}\">"._STATUS."</th><th class=\"{sorter: false}\">"._FUNCTIONS."</th></tr></thead><tbody>";
-		while (list($id, $title, $infotext, $url, $num, $hits, $prod_id, $status) = $db->sql_fetchrow($result)) {
-			$act = ($status) ? 0 : 1;
-			$time = (file_exists("uploads/clients/".$url)) ? date(_TIMESTRING, filemtime("uploads/clients/".$url)) : _NO_INFO;
-			$cont .= "<tr>"
-			."<td>".$id."</td>"
-			."<td>".$title."</td>"
-			."<td>".$num."</td>"
-			."<td>".$time."</td>"
-			."<td>".$prod_id."</td>"
-			."<td>".$hits."</td>"
-			."<td>".ad_status("", $status)."</td>"
-			."<td>".add_menu(ad_status($admin_file.".php?op=clients_active&amp;id=".$id."&amp;act=".$act, $status)."||<a href=\"".$admin_file.".php?op=clients_add&amp;id=".$id."\" title=\""._FULLEDIT."\">"._FULLEDIT."</a>||<a href=\"".$admin_file.".php?op=clients_delete&amp;id=".$id."\" OnClick=\"return DelCheck(this, '"._DELETE." &quot;".$title."&quot;?');\" title=\""._ONDELETE."\">"._ONDELETE."</a>")."</td></tr>";
-		}
-		$cont .= "</tbody></table>";
-		$cont .= tpl_eval('close', '');
-	} else {
-		$cont .= setTemplateWarning('warn', array('time' => '', 'url' => '', 'id' => 'info', 'text' => _NO_INFO));
-	}
-	echo $cont;
-	foot();
+function clients(): void {
+    global $db, $afile, $stop;
+    head();
+    $cont = navi(0, 0, 0, 0);
+    if ($stop) $cont .= setTemplateWarning('warn', ['time' => '', 'url' => '', 'id' => 'warn', 'text' => _CERROR]);
+    $result = $db->sql_query('SELECT id, title, infotext, url, num, hits, prod_id, status FROM '.PREFIX_DB.'_clients_down');
+    if ($db->sql_numrows($result) > 0) {
+        $cont .= setTemplateBasic('open');
+        $cont .= '<table class="sl_table_list_sort"><thead><tr><th>'._ID.'</th><th>'._CTITLE.'</th><th>'._CVERSION.'</th><th>'._CDATE.'</th><th>'._ID.'</th><th>'._CLOADS.'</th><th class="{sorter: false}">'._STATUS.'</th><th class="{sorter: false}">'._FUNCTIONS.'</th></tr></thead><tbody>';
+        while ([$id, $title, $infotext, $url, $num, $hits, $prod_id, $status] = $db->sql_fetchrow($result)) {
+            $act = ($status) ? 0 : 1;
+            $time = (file_exists('uploads/clients/'.$url)) ? date(_TIMESTRING, filemtime('uploads/clients/'.$url)) : _NO_INFO;
+            $cont .= '<tr>'
+            .'<td>'.$id.'</td>'
+            .'<td>'.$title.'</td>'
+            .'<td>'.$num.'</td>'
+            .'<td>'.$time.'</td>'
+            .'<td>'.$prod_id.'</td>'
+            .'<td>'.$hits.'</td>'
+            .'<td>'.ad_status('', $status).'</td>'
+            .'<td>'.add_menu(ad_status($afile.'.php?name=clients&amp;op=status&amp;id='.$id.'&amp;act='.$act, $status)
+            .'||<a href="'.$afile.'.php?name=clients&amp;op=add&amp;id='.$id.'" title="'._FULLEDIT.'">'._FULLEDIT.'</a>'
+            .'||<a href="'.$afile.'.php?name=clients&amp;op=del&amp;id='.$id.'" OnClick="return DelCheck(this, \''._DELETE.' &quot;'.$title.'&quot;?\');" title="'._ONDELETE.'">'._ONDELETE.'</a>')
+            .'</td></tr>';
+        }
+        $cont .= '</tbody></table>';
+        $cont .= setTemplateBasic('close');
+    } else {
+        $cont .= setTemplateWarning('warn', ['time' => '', 'url' => '', 'id' => 'info', 'text' => _NO_INFO]);
+    }
+    echo $cont;
+    foot();
 }
 
-function clients_add() {
-	global $db, $admin_file, $stop;
-	if (isset($_REQUEST['id'])) {
-		$cid = getVar('req', 'id', 'num');
-		$result = $db->sql_query('SELECT title, infotext, url, num, code, prod_id, status FROM '.PREFIX_DB.'_clients_down WHERE id = :cid', ['cid' => $cid]);
-		list($title, $infotext, $url, $num, $code, $prod_id, $status) = $db->sql_fetchrow($result);
-	} else {
-		$cid = getVar('post', 'cid', 'num');
-		$title = save_text(getVar('post', 'title', 'text'), 1);
-		$infotext = save_text(getVar('post', 'infotext', 'text'));
-		$url = getVar('post', 'url', 'text');
-		$num = getVar('post', 'num', 'text');
-		$code = getVar('post', 'code', 'text');
-		$prod_id = getVar('post', 'prod_id', 'num');
-		$status = getVar('post', 'status', 'num');
-	}
-	head();
-	$cont = clients_navi(0, 1, 0, 0);
-	if ($stop) $cont .= tpl_warn("warn", $stop, "", "", "warn");
-	if ($infotext) $cont .= preview($title, $infotext, "", "", "all");
-	$cont .= tpl_eval("open");
-	$cont .= "<form name=\"post\" action=\"".$admin_file.".php\" method=\"post\"><table class=\"sl_table_form\">"
-	."<tr><td>"._CTITLE.":</td><td><input type=\"text\" name=\"title\" value=\"".$title."\" maxlength=\"100\" class=\"sl_form\" placeholder=\""._CTITLE."\" required></td></tr>"
-	."<tr><td>"._TEXT.":</td><td>".textarea("1", "infotext", $infotext, "clients", "15", _TEXT, "1")."</td></tr>"
-	."<tr><td>"._CURL.":</td><td><input type=\"text\" name=\"url\" value=\"".$url."\" maxlength=\"100\" class=\"sl_form\" placeholder=\""._CURL."\"></td></tr>"
-	."<tr><td>"._CVERSION.":</td><td><input type=\"text\" name=\"num\" value=\"".$num."\" maxlength=\"10\" class=\"sl_form\" placeholder=\""._CVERSION."\"></td></tr>"
-	."<tr><td>"._CODE.":</td><td><input type=\"text\" name=\"code\" value=\"".$code."\" maxlength=\"100\" class=\"sl_form\" placeholder=\""._CODE."\"></td></tr>"
-	."<tr><td>"._ID.":</td><td><input type=\"number\" name=\"prod_id\" value=\"".$prod_id."\" class=\"sl_form\" placeholder=\""._ID."\"></td></tr>"
-	."<tr><td>"._CADOWN."</td><td>".radio_form($status, "status")."</td></tr>"
-	."<tr><td colspan=\"2\" class=\"sl_center\">".ad_save("cid", $cid, "clients_save")."</td></tr></table></form>";
-	$cont .= tpl_eval('close', '');
-	echo $cont;
-	foot();
+function add(): void {
+    global $db, $afile, $stop;
+    $id = getVar('req', 'id', 'num');
+    if ($id) {
+        $result = $db->sql_query('SELECT id, title, infotext, url, num, code, prod_id, status FROM '.PREFIX_DB.'_clients_down WHERE id = :id', ['id' => $id]);
+        [$cid, $title, $infotext, $url, $num, $code, $prod_id, $status] = $db->sql_fetchrow($result);
+    } else {
+        $cid = getVar('post', 'cid', 'num');
+        $title = save_text(getVar('post', 'title', 'title', ''), 1);
+        $infotext = save_text(getVar('post', 'infotext', 'text', ''));
+        $url = getVar('post', 'url', 'text', '');
+        $num = getVar('post', 'num', 'text', '');
+        $code = getVar('post', 'code', 'text', '');
+        $prod_id = getVar('post', 'prod_id', 'num', 0);
+        $status = getVar('post', 'status', 'num', 0);
+    }
+    head();
+    $cont = navi(0, 1, 0, 0);
+    if ($stop) {
+        $stopText = is_array($stop) ? implode('<br>', $stop) : $stop;
+        $cont .= setTemplateWarning('warn', ['time' => '', 'url' => '', 'id' => 'warn', 'text' => $stopText]);
+    }
+    if ($infotext) $cont .= preview($title, $infotext, '', '', 'all');
+    $cont .= setTemplateBasic('open');
+    $cont .= '<form name="post" action="'.$afile.'.php" method="post"><table class="sl_table_form">'
+    .'<tr><td>'._CTITLE.':</td><td><input type="text" name="title" value="'.$title.'" maxlength="100" class="sl_form" placeholder="'._CTITLE.'" required></td></tr>'
+    .'<tr><td>'._TEXT.':</td><td>'.textarea('1', 'infotext', $infotext, 'clients', '15', _TEXT, '1').'</td></tr>'
+    .'<tr><td>'._CURL.':</td><td><input type="text" name="url" value="'.$url.'" maxlength="100" class="sl_form" placeholder="'._CURL.'"></td></tr>'
+    .'<tr><td>'._CVERSION.':</td><td><input type="text" name="num" value="'.$num.'" maxlength="10" class="sl_form" placeholder="'._CVERSION.'"></td></tr>'
+    .'<tr><td>'._CODE.':</td><td><input type="text" name="code" value="'.$code.'" maxlength="100" class="sl_form" placeholder="'._CODE.'"></td></tr>'
+    .'<tr><td>'._ID.':</td><td><input type="number" name="prod_id" value="'.$prod_id.'" class="sl_form" placeholder="'._ID.'"></td></tr>'
+    .'<tr><td>'._CADOWN.'</td><td>'.radio_form($status, 'status').'</td></tr>'
+    .'<tr><td colspan="2" class="sl_center"><input type="hidden" name="name" value="clients">'.ad_save('cid', $cid, 'save').'</td></tr></table></form>';
+    $cont .= setTemplateBasic('close');
+    echo $cont;
+    foot();
 }
 
-function clients_save() {
-	global $db, $admin_file, $stop;
-	$cid = getVar('post', 'cid', 'num');
-	$title = save_text(getVar('post', 'title', 'text'), 1);
-	$infotext = save_text(getVar('post', 'infotext', 'text'));
-	$url = getVar('post', 'url', 'text');
-	$num = getVar('post', 'num', 'text');
-	$code = getVar('post', 'code', 'text');
-	$prod_id = getVar('post', 'prod_id', 'num');
-	$status = getVar('post', 'status', 'num');
-	if (!$title) $stop[] = _CERROR;
-	if (!$infotext) $stop[] = _CERROR1;
-	$posttype = getVar('post', 'posttype', 'text');
-	if (!$stop && $posttype == "save") {
-		if ($cid) {
-			$db->sql_query('UPDATE '.PREFIX_DB.'_clients_down SET title = :title, infotext = :infotext, url = :url, num = :num, code = :code, prod_id = :prod_id, status = :status WHERE id = :cid', ['title' => $title, 'infotext' => $infotext, 'url' => $url, 'num' => $num, 'code' => $code, 'prod_id' => $prod_id, 'status' => $status, 'cid' => $cid]);
-		} else {
-			$db->sql_query('INSERT INTO '.PREFIX_DB.'_clients_down VALUES (NULL, :title, :infotext, :url, :num, :code, \'0\', :prod_id, :status)', ['title' => $title, 'infotext' => $infotext, 'url' => $url, 'num' => $num, 'code' => $code, 'prod_id' => $prod_id, 'status' => $status]);
-		}
-		header("Location: ".$admin_file.".php?op=clients");
-	} elseif ($posttype == "delete") {
-		clients_delete($cid);
-	} else {
-		clients_add();
-	}
+function save(): void {
+    global $db, $afile, $stop;
+    $cid = getVar('post', 'cid', 'num', 0);
+    $title = save_text(getVar('post', 'title', 'title', ''), 1);
+    $infotext = save_text(getVar('post', 'infotext', 'text', ''));
+    $url = getVar('post', 'url', 'text', '');
+    $num = getVar('post', 'num', 'text', '');
+    $code = getVar('post', 'code', 'text', '');
+    $prod_id = getVar('post', 'prod_id', 'num', 0);
+    $status = getVar('post', 'status', 'num', 0);
+    $stop = [];
+    if (!$title) $stop[] = _CERROR;
+    if (!$infotext) $stop[] = _CERROR1;
+    $posttype = getVar('post', 'posttype', 'var', '');
+    if (!$stop && $posttype === 'save') {
+        if ($cid) {
+            $db->sql_query('UPDATE '.PREFIX_DB.'_clients_down SET title = :title, infotext = :infotext, url = :url, num = :num, code = :code, prod_id = :prod_id, status = :status WHERE id = :id', ['title' => $title, 'infotext' => $infotext, 'url' => $url, 'num' => $num, 'code' => $code, 'prod_id' => $prod_id, 'status' => $status, 'id' => $cid]);
+        } else {
+            $db->sql_query('INSERT INTO '.PREFIX_DB.'_clients_down (title, infotext, url, num, code, hits, prod_id, status) VALUES (:title, :infotext, :url, :num, :code, :hits, :prod_id, :status)', ['title' => $title, 'infotext' => $infotext, 'url' => $url, 'num' => $num, 'code' => $code, 'hits' => 0, 'prod_id' => $prod_id, 'status' => $status]);
+        }
+        header('Location: '.$afile.'.php?name=clients');
+        exit;
+    } elseif ($posttype === 'delete') {
+        del($cid);
+    } else {
+        add();
+    }
 }
 
-function clients_delete() {
-	global $db, $admin_file, $id;
-	$arg = func_get_args();
-	$id = (!empty($arg[0])) ? $arg[0] : getVar('req', 'id', 'num');
-	if ($id) $db->sql_query('DELETE FROM '.PREFIX_DB.'_clients_down WHERE id = :id', ['id' => $id]);
-	header("Location: ".$admin_file.".php?op=clients");
+function del(int $id = 0): void {
+    global $db, $afile;
+    if (!$id) $id = getVar('req', 'id', 'num');
+    if ($id) $db->sql_query('DELETE FROM '.PREFIX_DB.'_clients_down WHERE id = :id', ['id' => $id]);
+    header('Location: '.$afile.'.php?name=clients');
+    exit;
 }
 
-function clients_info() {
-	head();
-	echo clients_navi(0, 2, 0, 0)."<div id=\"repadm_info\">".adm_info(1, "clients", 0)."</div>";
-	foot();
+function status(): void {
+    global $db, $afile;
+    $id = getVar('get', 'id', 'num');
+    $act = getVar('get', 'act', 'num');
+    if ($id) $db->sql_query('UPDATE '.PREFIX_DB.'_clients_down SET status = :status WHERE id = :id', ['status' => $act, 'id' => $id]);
+    header('Location: '.$afile.'.php?name=clients');
+    exit;
 }
 
-switch($op) {
-	case "clients":
-	clients();
-	break;
-
-	case "clients_add":
-	clients_add();
-	break;
-	
-	case "clients_save":
-	clients_save();
-	break;
-	
-	case "clients_active":
-	$id = getVar('req', 'id', 'num');
-	$act = getVar('req', 'act', 'num');
-	$db->sql_query('UPDATE '.PREFIX_DB.'_clients_down SET status = :act WHERE id = :id', ['act' => $act, 'id' => $id]);
-	header("Location: ".$admin_file.".php?op=clients");
-	break;
-	
-	case "clients_delete":
-	clients_delete();
-	break;
-	
-	case "clients_info":
-	clients_info();
-	break;
+function info(): void {
+    head();
+    echo navi(0, 2, 0, 0).'<div id="repadm_info">'.adm_info(1, 'clients', 0).'</div>';
+    foot();
 }
-?>
+
+switch ($op) {
+    default: clients(); break;
+    case 'add': add(); break;
+    case 'save': save(); break;
+    case 'status': status(); break;
+    case 'del': del(); break;
+    case 'info': info(); break;
+}
